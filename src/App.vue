edd1a399c49c1d88ae39c91b5f8bbdbf9a425f86
samantha <samanthapelkova1@gmail.com>
2024-03-21 15:12:42 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           class="product-item"
           @add-to-basket="addToBasket(product, $event)"
       />
-      <note-to-buy class="notesBlock"></note-to-buy>
+      <note-to-buy class="notesBlock"/>
     </div>
   </div>
 </template>
